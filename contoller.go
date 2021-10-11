920df8653a0de4c74c7f909df5ab5e51b1c2d4e2
kritika.singhal <3319-kritika.singhal@users.noreply.source.golabs.io>
2021-10-11 14:35:55 +0530

    Feat: Add function to create service whenever deployment is created

--- contoller.go
+++ contoller.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	appsinformer "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
@@ -28,20 +32,22 @@ func newController(clientset kubernetes.Interface, depInformer appsinformer.Depl
 
 	depInformer.Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
-			AddFunc:    handleAdd,
-			DeleteFunc: handleDel,
+			AddFunc:    c.handleAdd,
+			DeleteFunc: c.handleDel,
 		},
 	)
 
 	return c
 }
 
-func handleAdd(obj interface{}) {
+func (c *controller) handleAdd(obj interface{}) {
 	fmt.Println("Add was called")
+	c.queue.Add(obj)
 }
 
-func handleDel(obj interface{}) {
+func (c *controller) handleDel(obj interface{}) {
 	fmt.Println("Delete was called")
+	c.queue.Add(obj)
 }
 
 func (c *controller) run(ch <-chan struct{}) {
@@ -54,5 +60,66 @@ func (c *controller) run(ch <-chan struct{}) {
 }
 
 func (c *controller) worker() {
+	for c.processItem() {
 
+	}
+}
+
+func (c *controller) processItem() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+	defer c.queue.Forget(item)
+	key, err := cache.MetaNamespaceKeyFunc(item)
+	if err != nil {
+		fmt.Println("Error getting key from cache %s\n", err.Error())
+	}
+
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		fmt.Println("Error in Splitting key into name space and name %s\n", err.Error())
+	}
+	err = c.syncDeployment(ns, name)
+	if err != nil {
+		fmt.Println("Error Syncing deployment %s\n", err.Error())
+		return false
+	}
+	return true
+}
+
+func (c *controller) syncDeployment(ns, name string) error {
+	ctx := context.Background()
+
+	dep, err := c.deplister.Deployments(ns).Get(name)
+	if err != nil {
+		fmt.Println("Error getting from lister %s\n", err.Error())
+	}
+
+	//create service
+	// we have to modify this to figure out the port our deployment's container is listening on
+	svc := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      dep.Name,
+			Namespace: dep.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: depLabels(*dep),
+			Ports: []corev1.ServicePort{
+				{
+					Name: "http",
+					Port: 80,
+				},
+			},
+		},
+	}
+	_, err = c.clientset.CoreV1().Services(ns).Create(ctx, &svc, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Println("Error in creating service %s\n", err.Error())
+	}
+	return nil
+}
+
+func depLabels(dep appsv1.Deployment) map[string]string {
+	return dep.Spec.Template.Labels
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	golang.org/x/net v0.0.0-20211005215030-d2e5035098b3 // indirect
 	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	k8s.io/api v0.20.0
 	k8s.io/apimachinery v0.22.2
 	k8s.io/client-go v0.20.0
 	k8s.io/klog v1.0.0 // indirect
